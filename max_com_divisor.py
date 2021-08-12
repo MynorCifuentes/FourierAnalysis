84326294ea5d027643fd035e1f33b7eb2c9d7d60
Mynor Cifuentes <mynoracifuentes09@gmail.com>
2021-08-12 09:06:58 -0600

    declarado frecuencias y coeficientes

---
+++ max_com_divisor.py
@@ -0,0 +1,7 @@
+
+#ingereso de frecuencias y coeficientes de x(t)
+
+frecs = [50, 100, 150]  #frecuencias de menor a mayor
+coeficientes = [3, -4, 0.7] #coeficientes según frecuencias correspondientes
+
+
