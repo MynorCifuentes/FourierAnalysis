ce8183aa39e1c94f67ffdb4880be9cc92e20dfd2
Mynor Cifuentes <mynoracifuentes09@gmail.com>
2021-08-12 10:23:34 -0600

    Grafica de las señales

--- max_com_divisor.py
+++ max_com_divisor.py
@@ -1,4 +1,6 @@
-
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 #ingereso de frecuencias y coeficientes de x(t)
 
 frecs = [25,50, 100, 150,200]  #frecuencias de menor a mayor
@@ -36,3 +38,28 @@ periodos = [1./k for k in frecs]
 for i in range(len(frecs)):
     print('%5.1f %12d %14.3f %7d' %(amplitudes[i], frecs[i], periodos[i], armonicas[i]))
 print("")
+
+#El siguiente código sirve para graficar
+
+fig, g = plt.subplots()
+t = np.linspace(0,2./frecuencia,100)
+for k in range(len(frecs)):
+    g.plot(t, coeficientes[k]*np.sin(2*np.pi*frecs[k]*t), label="n={0}".format(armonicas[k]))
+plt.xlabel('t, seg')
+plt.ylabel('Amplitudes')
+plt.title('Armónicas')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+s = [0]*100
+for k in range(len(frecs)):
+    t = np.linspace(0, 2./frecuencia, 100)
+    sp = (coeficientes[k]*np.sin(2*np.pi*frecs[k]*t))
+    s += sp
+plt.plot(t,s,'k')
+plt.title('x(t)')
+plt.xlabel('t, seg')
+plt.ylabel('x(t)')
+plt.grid(True)
+plt.show()
